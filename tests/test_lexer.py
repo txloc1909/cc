131381b1eec03754a1db8ee3c9fa999c79aa7408
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2025-05-25 01:10:10 +0700

    lexer unittest

---
+++ tests/test_lexer.py
@@ -0,0 +1,66 @@
+import pytest
+
+from cc.lexer import lex
+
+
+def test_empty_src():
+    assert lex("") == []
+
+
+@pytest.mark.xfail
+def test_single_char():
+    src = "(){},.-+;*"
+    assert lex(src) == [
+        ("LPAREN", "("),
+        ("RPAREN", ")"),
+        ("LBRACE", "{"),
+        ("RBRACE", "}"),
+        ("COMMA", ","),
+        ("DOT", "."),
+        ("MINUS", "-"),
+        ("PLUS", "+"),
+        ("SEMICOLON", ";"),
+        ("STAR", "*"),
+    ]
+
+
+@pytest.mark.xfail
+def test_one_or_two_chars():
+    src = "! != = == < <= > >= && ||"
+    assert lex(src) == [
+        ("BANG", "!"),
+        ("BANG_EQUAL", "!="),
+        ("EQUAL", "="),
+        ("EQUAL_EQUAL", "=="),
+        ("LESS", "<"),
+        ("LESS_EQUAL", "<="),
+        ("GREATER", ">"),
+        ("GREATER_EQUAL", ">="),
+        ("AND", "&&"),
+        ("OR", "||"),
+    ]
+
+
+def test_keywords():
+    src = "int void return"  # TODO add more
+    assert lex(src) == [
+        ("KEYWORD", "int"),
+        ("KEYWORD", "void"),
+        ("KEYWORD", "return"),
+    ]
+
+
+def test_comments():
+    src = """
+// this is a comment
+void // commend after keyword
+/* multiline
+comment */
+"""
+
+    assert lex(src) == [
+        ("COMMENT", "// this is a comment"),
+        ("KEYWORD", "void"),
+        ("COMMENT", "// commend after keyword"),
+        ("COMMENT", "/* multiline\ncomment */"),
+    ]
